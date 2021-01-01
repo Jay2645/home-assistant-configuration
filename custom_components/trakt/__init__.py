279731e5bf99186992ca4199c0e1aac2bc8dd10f
root <root@core-ssh.local.hass.io>
2020-12-31 18:00:16 -0800

    Big update, realized I lost connection to git

--- custom_components/trakt/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Home Assistant component to feed the Upcoming Media Lovelace card with
-Trakt user's upcoming TV episodes.
-
-https://github.com/custom-components/sensor.trakt
-
-https://github.com/custom-cards/upcoming-media-card
-"""
--- www/community/lovelace-card-tools-2.3/src/more-info.js
+++
@@ -1,8 +0,0 @@
-import { fireEvent } from "./event.js";
-
-export function moreInfo(entity, large=false) {
-  fireEvent("hass-more-info", {entityId: entity}, document.querySelector("home-assistant"));
-  const el = document.querySelector("home-assistant")._moreInfoEl;
-  el.large = large;
-  return el;
-}
